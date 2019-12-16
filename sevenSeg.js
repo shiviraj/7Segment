21e26d108b4301fa9651431461da194abe84834d
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2019-12-16 20:11:32 +0530

    refactor code

--- sevenSeg.js
+++ sevenSeg.js
@@ -1,29 +1,27 @@
-"use strict";
-const Gpio = require("onoff").Gpio;
+'use strict';
+const { Gpio } = require('onoff');
 
-const sleep = function (time) {
+const sleep = function(time) {
   let newTime = new Date().getTime() + time;
   while (newTime > new Date().getTime());
 };
 
-const setPin = function (pinNo, mode) {
+const setPin = function(pinNo, mode) {
   return new Gpio(pinNo, mode);
 };
 
-const setBoard = function (pins, mode) {
-  const assignedPins = pins.map(function (pin) {
-    return setPin(pin, mode);
-  });
+const setBoard = function(pins, mode) {
+  const assignedPins = pins.map(pin => setPin(pin, mode));
   return assignedPins;
 };
 
-const seg = function (pins, val) {
+const seg = function(pins, val) {
   for (let i = 0; i < 8; i++) {
     pins[i].writeSync((val >> i) & 1);
   }
 };
 
-const main = function () {
+const main = function() {
   const pins = [5, 6, 13, 19, 26, 16, 20, 21];
   const numbers = [0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90];
   const segment = setBoard(pins, 'out');
