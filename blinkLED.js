8bf202467fa061133abe82170193b8bfdf559873
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2019-12-21 11:27:17 +0530

    refactor code

--- blinkLED.js
+++ blinkLED.js
@@ -1,29 +1,29 @@
-"use strict";
-const Gpio = require("onoff").Gpio;
+'use strict';
+const { Gpio } = require('onoff');
 
-const sleep = function (time) {
-    let newTime = new Date().getTime() + time;
-    while (newTime > new Date().getTime());
+const sleep = function(time) {
+  let newTime = new Date().getTime() + time;
+  while (newTime > new Date().getTime());
 };
 
-const setBoard = function (pinNo, mode) {
-    return new Gpio(pinNo, mode);
+const setBoard = function(pinNo, mode) {
+  return new Gpio(pinNo, mode);
 };
 
-const main = function () {
-    const LED = setBoard(4, 'out');
-    const button = setBoard(17, 'in');
-    let number = 0;
-    while (true) {
-        if (button.readSync() == 1) {
-            while (button.readSync() == 1);
-            number++;
-            console.log(number)
-            LED.writeSync(1);
-            sleep(500)
-            LED.writeSync(0);
-        }
+const main = function() {
+  const LED = setBoard(4, 'out');
+  const button = setBoard(17, 'in');
+  let number = 0;
+  while (true) {
+    if (button.readSync() == 1) {
+      while (button.readSync() == 1);
+      number++;
+      console.log(number);
+      LED.writeSync(1);
+      sleep(500);
+      LED.writeSync(0);
     }
+  }
 };
 
 main();
